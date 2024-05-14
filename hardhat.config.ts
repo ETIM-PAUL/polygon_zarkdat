5d315e4e38c02a2aab8ddb9215ece0fa06be3274
etim <etimpaul22@gmail.com>
2024-05-14 16:48:06 +0100

    added circuit, deployed and verified input

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,13 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 // https://github.com/projectsophon/hardhat-circom
 import "hardhat-circom";
 // circuits
-import circuits = require('./circuits.config.json')
-
+import circuits = require("./circuits.config.json");
+import "dotenv/config";
 // set env var to the root of the project
-process.env.BASE_PATH = __dirname;
+// process.env.BASE_PATH = __dirname;
 
 // tasks
-import "./tasks/newcircuit.ts"
+import "./tasks/newcircuit.ts";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -19,8 +19,14 @@ const config: HardhatUserConfig = {
       },
       {
         version: "0.6.11",
-      }
-    ]
+      },
+    ],
+  },
+  networks: {
+    amoy: {
+      url: process.env.AMOY_RPC,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    },
   },
   circom: {
     // (optional) Base path for input files, defaults to `./circuits/`
@@ -28,7 +34,7 @@ const config: HardhatUserConfig = {
     // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
     ptau: "powersOfTau28_hez_final_12.ptau",
     // (required) Each object in this array refers to a separate circuit
-    circuits: JSON.parse(JSON.stringify(circuits))
+    circuits: JSON.parse(JSON.stringify(circuits)),
   },
 };
 
